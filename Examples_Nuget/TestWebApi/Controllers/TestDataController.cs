fbd03a49765551cd47fc777fa9668d10bcda55c7
bdeng-xt <bdeng.xt@gmail.com>
2022-01-12 10:53:21 -0500

    initial version of webapi

---
+++ Examples_Nuget/TestWebApi/Controllers/TestDataController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using TestWebApi.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TestWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestDataController : ControllerBase
+    {
+        private List<TestWebApi.Models.TestData> persons = new List<TestWebApi.Models.TestData> {
+            new TestWebApi.Models.TestData {Id=1,FirstName="Peter",LastName="Peterson"},
+            new TestWebApi.Models.TestData {Id=2,FirstName="Tom",LastName="Thomson"},
+            new TestWebApi.Models.TestData {Id=3,FirstName="John",LastName="Johnson"}
+        };
+
+        // GET: api/<TestDataController>
+        [HttpGet]
+        public ActionResult<IEnumerable<TestWebApi.Models.TestData>> Get()
+        {
+            return persons;
+        }
+
+        // GET api/<TestDataController>/5
+        [HttpGet("{id}")]
+        public ActionResult<TestData> Get(int id)
+        {
+            TestData person = persons.FirstOrDefault(c => c.Id == id);
+            if (person == null)
+            {
+                return NotFound(new { Message = "not found" });
+            }
+            return Ok(person);
+        }
+
+        // POST api/<TestDataController>
+        [HttpPost]
+        public ActionResult<IEnumerable<TestData>> Post(TestData newdata)
+        {
+            persons.Add(newdata);
+            return persons;
+        }
+
+        // PUT api/<TestDataController>/5
+        [HttpPut("{id}")]
+        public ActionResult<IEnumerable<TestData>> Put(int id, TestData updatedata)
+        {
+            TestData person = persons.FirstOrDefault(c => c.Id == id);
+            if (person == null)
+            {
+                return NotFound(new { Message = "not found" });
+            }
+            person.FirstName = updatedata.FirstName;
+            person.LastName = updatedata.LastName;
+            person.IsDeleted = updatedata.IsDeleted;
+            return persons;
+        }
+
+        // DELETE api/<TestDataController>/5
+        [HttpDelete("{id}")]
+        public ActionResult<IEnumerable<TestData>> Delete(int id)
+        {
+            TestData person = persons.FirstOrDefault(c => c.Id == id);
+            if (person == null)
+            {
+                return NotFound(new { Message = "not found" });
+            }
+            persons.Remove(person);
+            return persons;
+        }
+    }
+}
---
+++ Examples_Nuget/TestWebApi/Models/TestData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestWebApi.Models
+{
+    public class TestData
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+
+        public bool IsDeleted { get; set; } = false;
+    }
+}
