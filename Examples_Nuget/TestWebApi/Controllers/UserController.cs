d1dfd830782ab24fc91553f2d47bfaed553ee64d
bdeng-xt <bdeng.xt@gmail.com>
2022-01-12 10:53:21 -0500

    initial version of webapi

---
+++ Examples_Nuget/TestWebApi/Controllers/UserController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TestWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        // GET: api/<UserController>
+        [HttpGet]
+        public ActionResult<TileDB.Cloud.Rest.Model.User> Get()
+        {
+            TileDB.Cloud.Rest.Model.User user = TileDB.Cloud.RestUtil.GetUser();
+            if (user == null)
+            {
+                return NotFound(new { Message = "no user found" });
+            }
+             
+            return user;
+ 
+        }
+
+        // GET api/<UserController>/5
+        [HttpGet("{id}")]
+        public ActionResult<TileDB.Cloud.Rest.Model.User> Get(int id)
+        {
+            TileDB.Cloud.Rest.Model.User user = new TileDB.Cloud.Rest.Model.User(username: "testuser");
+            return user;
+        }
+
+        // POST api/<UserController>
+        [HttpPost]
+        public ActionResult<TileDB.Cloud.Rest.Model.User> Post([FromBody] string value)
+        {
+            return NotFound(new { Message = "Not supported operation." });
+        }
+
+        // PUT api/<UserController>/5
+        [HttpPut("{id}")]
+        public ActionResult<TileDB.Cloud.Rest.Model.User> Put(int id, [FromBody] string value)
+        {
+            return NotFound(new { Message = "Not supported operation." });
+        }
+
+        // DELETE api/<UserController>/5
+        [HttpDelete("{id}")]
+        public ActionResult<TileDB.Cloud.Rest.Model.User> Delete(int id)
+        {
+            return NotFound(new { Message = "Not supported operation." });
+        }
+    }
+}
---
+++ Examples_Nuget/TestWebApi/TestTileDBUtil.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestWebApi
+{
+    public class TestTileDBUtil
+    {
+        public static void Login()
+        {
+            string tiledb_rest_host = System.Environment.GetEnvironmentVariable("TILEDB_REST_HOST");
+            if(string.IsNullOrEmpty(tiledb_rest_host))
+            {
+                tiledb_rest_host = "https://api.tiledb.com";
+            }
+            string tiledb_rest_token = System.Environment.GetEnvironmentVariable("TILEDB_REST_TOKEN");
+            if (string.IsNullOrEmpty(tiledb_rest_token))
+            {
+                tiledb_rest_token = "";//set default token here
+            }
+
+            TileDB.Cloud.Client.Login(token: tiledb_rest_token, host: tiledb_rest_host);
+        }
+    }
+}
