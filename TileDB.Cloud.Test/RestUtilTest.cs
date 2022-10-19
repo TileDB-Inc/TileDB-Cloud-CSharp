9da4917aca6f0e37db5d46307dd12934773256bc
Theodore Tsirpanis <theodore.tsirpanis@tiledb.com>
2022-10-19 13:04:44 +0300

    Make the examples build.

--- TileDB.Cloud.Test/RestUtilTest.cs
+++ TileDB.Cloud.Test/RestUtilTest.cs
@@ -1,7 +1,6 @@
 using System;
-using System.IO;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using TileDB.Cloud;
+
 namespace TileDB.Cloud.Test
 {
     [TestClass]
@@ -11,15 +10,12 @@ namespace TileDB.Cloud.Test
         public void TestSplitUri()
         {
             string uri1 = "tiledb://user1/array_name";
-            var (name_space_1, array_path1) = TileDB.Cloud.RestUtil.SplitUri(uri1);
-            Assert.AreEqual<string>("user1", name_space_1);
-            Assert.AreEqual<string>("array_name", array_path1);
+            var (name_space_1, array_path1) = RestUtil.SplitUri(uri1);
+            Assert.AreEqual("user1", name_space_1);
+            Assert.AreEqual("array_name", array_path1);
 
             string uri2 = "s3://user2/array_name2";
-            Assert.ThrowsException<ArgumentException>(()=>TileDB.Cloud.RestUtil.SplitUri(uri2));
-
+            Assert.ThrowsException<ArgumentException>(() => RestUtil.SplitUri(uri2));
         }
-
-
     }
 }
